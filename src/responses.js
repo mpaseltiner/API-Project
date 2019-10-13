f4ee0f0f11c7e47421844ad2586e65e1c6fa8d1a
mpaseltiner <mjp3591@rit.edu>
2019-10-13 18:50:53 -0400

    project done

--- src/responses.js
+++ src/responses.js
@@ -1,4 +1,19 @@
-const drinks = {};
+const drinks = {
+  'The Lemon': {
+    name: 'The Lemon',
+    image: 'https://i0.wp.com/images-prod.healthline.com/hlcmsresource/images/AN_images/lemon-health-benefits-1296x728-feature.jpg?w=1155',
+    ingredients: 'tons of lemons and ice',
+    glass: 'Rocks',
+    instructions: 'squeeze the lemons into the rocks glass add ice and stir until chilled',
+  },
+  'The Manhattan': {
+    name: "The Manhattan",
+    image: "https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/2/13/0/ED0309H_classic-manhattan-cocktail_s4x3.jpg.rend.hgtvcom.826.620.suffix/1371614573383.jpeg",
+    ingredients: "Ice\n2 oz rye whiskey\n1/2 oz sweet vermouth\n1 dash Angostura bitters\n1 maraschino cherry",
+    glass: "Rocks",
+    instructions: "Combine whiskey, vermouth, and bitters in a cocktail mixing glass. Add ice and stir until chilled. Strain into a chilled rocks glass. Garnish with the maraschino cherry",
+  },
+};
 
 const respondJSON = (request, response, status, object) => {
   response.writeHead(status, { 'Content-Type': 'application/json' });
